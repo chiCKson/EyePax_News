a271baf7e14463569d07c10dda9f520623d8e431
erandra <erandra@mihcm.com>
2022-06-10 23:48:53 +0530

    Initial commit

---
+++ app/src/main/java/com/chickson/eyepaxnews/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.chickson.eyepaxnews.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryRed = Color(0xFFFF3A44)
+val SecondaryBlue = Color(0xFF0080FF)
+val TertiaryYellow = Color(0xFFFFE600)
+val Teal200 = Color(0xFF03DAC5)
---
+++ app/src/main/java/com/chickson/eyepaxnews/ui/theme/Theme.kt
@@ -0,0 +1,37 @@
+package com.chickson.eyepaxnews.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+
+
+private val ColorPalette = lightColors(
+    primary = PrimaryRed,
+    primaryVariant = TertiaryYellow,
+    secondary = SecondaryBlue,
+
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+@Composable
+fun EyePaxNewsTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val colors = ColorPalette
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
